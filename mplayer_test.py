41a13a3d987b19cb4b1c735bcdf90b1b06a292b6
Philip Armour <philiparmour@yahoo.co.uk>
2024-04-17 17:11:03 +0100

    experiment

--- mplayer_test.py
+++ mplayer_test.py
@@ -2,51 +2,62 @@
 
 import sys
 import os
-import json
-import argparse
+from gpiozero import Button
 import time
 from subprocess import Popen, PIPE, CalledProcessError
 
 mplayer = "/usr/bin/mplayer"
 
-#parse_mplayer_output = lambda x: x.split("A:")[-1].split("V:")[0].strip()
-parse_mplayer_output = lambda x: x.split("A:  ")[-1].split(" ")[0].strip()
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='mplayer-resume')
-    parser.add_argument('-r', '--resume', nargs=1, type=int, metavar='int', default=-2,
-            help='time difference in seconds, negative number means a rollback')
-    parser.add_argument('flags', nargs=argparse.REMAINDER, default=None,
-            metavar='margs', help='mplayer arguments')
+fifo_file = "/tmp/mplayer-%d" % (time.time())
+os.mkfifo(fifo_file)
 
-    return (vars(parser.parse_args()), parser)
 
-def run_mplayer(args):
-    fifo_file = "/tmp/mplayer-%d" % (time.time())
-    os.mkfifo(fifo_file)
-    cmd = [mplayer, '-slave',  '-playlist', '/home/philip/temp_playlist.txt','-input'  ,'file=%s' % (fifo_file)] + args
-    print(cmd)
+# GPIO BUTTONS GPIO BCM numbers (Physical pin numbers)
 
-    with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
-        for line in p.stdout:
-            print(line, end='') # process line here
+PLAY   = 12  
+NEXT   = 7 
+PREV   = 20 
 
-    if p.returncode != 0:
-        raise CalledProcessError(p.returncode, p.args)
 
+buttonPREV  = Button(PREV)
+buttonPLAY  = Button(PLAY)
+buttonNEXT  = Button(NEXT)
+
+def mplayerCommand(someCommand):
+    with open(fifo_file, 'w') as fifo:
+        fifo.write(someCommand)
 
 
 def main():
-    args, parser = parse_args()
-    resume = args['resume']
-    if type(resume) == list: resume = resume[0]
-    mflags = args['flags']
-    file_name = None
 
+    cmd = [mplayer, '-slave',  '-playlist', '/home/philip/temp_playlist.txt','-input'  ,'file=%s' % (fifo_file)] 
+    print(cmd)
 
-    print(mflags)
-    run_mplayer(mflags)
+    playCounter = 0
+
+    with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            playCounter += 1
+            #print(line, end='') # process line here
+            if "Playing" in str(line):
+                print("EVENT: playing")
+            if "A:  " in str(line):
+                if ((playCounter % 20) == 0):
+                    timeElapsed = line.split()[1]
+                    print("Time Elapsed: " + str(timeElapsed))
+            if buttonPLAY.is_pressed:
+                mplayerCommand("pause\n")
+
+            if buttonNEXT.is_pressed:
+                mplayerCommand(">")
+                
+            if buttonPREV.is_pressed:
+                mplayerCommand("<")
+
+    if p.returncode != 0:
+        raise CalledProcessError(p.returncode, p.args)
 
 
 if __name__ == '__main__':
